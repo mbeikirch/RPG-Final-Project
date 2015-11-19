e011c23a11ef161b4cf303d0e68b1e6886a33f75
Matt Beikirch <meb3191@rit.edu>
2015-11-19 18:26:31 -0500

    Messing with tables for the selection screen, also made it so the text in the name field disappears when you click in it.

--- src/SelectionScreen.java
+++ src/SelectionScreen.java
@@ -3,6 +3,8 @@
  */
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.font.TextAttribute;
 import java.util.ArrayList;
 import java.util.Hashtable;
@@ -10,7 +12,11 @@ import java.util.Map;
 
 public class SelectionScreen
 {
-   private JLabel bla;
+   private JTable jtClassInfo;
+   private final int TABLE_WIDTH = 3;
+   private final int TABLE_HEIGHT = 3;
+   private final String[] columnNames = {"bla", "Base Stat", "On Level Up"};
+   private final String[][] tableData = {{"987", "bla", "ble"}, {"sd","asdf","453"}};
 
    public SelectionScreen()
    {
@@ -19,7 +25,7 @@ public class SelectionScreen
       clsFrame.setSize(500,300);
 
       Font titleFont = new Font("Book Antiqua", Font.BOLD, 24);
-      Font statsFont = new Font("Apple Casual", Font.PLAIN, 14);
+      Font statsFont = new Font("Verdana", Font.PLAIN, 14);
 
       //making an arraylist to hold copies of each class in order to pull info from them(stats, abilities, etc.)
       ArrayList<Player> classList = new ArrayList<Player>();
@@ -28,23 +34,55 @@ public class SelectionScreen
       classList.add(new Rogue("Rogue"));
 
       //holds the class info(type, stats, abilities, etc.)
-      JPanel classInfoPanel = new JPanel();
+      JPanel classInfoPanel = new JPanel(new FlowLayout());
 
-      //will hold the next/prev buttons, the name text field, and the create button
-      JPanel bottomPanel = new JPanel();
+         jtClassInfo = new JTable(tableData, columnNames);
 
-      //textfield to hold the player name
-      JTextField jtfName = new JTextField("");
-      bottomPanel.add(jtfName);
+         //making it so you cannot select the cells
+         jtClassInfo.setColumnSelectionAllowed(false);
+         jtClassInfo.setRowSelectionAllowed(false);
+         jtClassInfo.setFocusable(false);
 
-      JButton jbPrev = new JButton("<=");
-      bottomPanel.add(jbPrev);
+         //changing the look of the table
+         jtClassInfo.setShowGrid(false);
+         jtClassInfo.setFont(statsFont);
 
-      JButton jbNext = new JButton("=>");
-      bottomPanel.add(jbNext);
+         classInfoPanel.add(new JScrollPane(jtClassInfo));
+
+      //will hold the selection panel and the name panel
+      JPanel bottomPanel = new JPanel(new GridLayout(2,0));
+
+         JPanel namePanel = new JPanel(new FlowLayout());
+            //textfield to hold the player name
+            JTextField jtfName = new JTextField("Fighter Name",15);
+
+            //clears the textfield when the user clicks in the name text field
+            jtfName.addFocusListener(new FocusListener()
+            {
+               public void focusGained(FocusEvent e) { jtfName.setText(""); }
+
+               public void focusLost(FocusEvent e) {}
+            });
+
+            namePanel.add(jtfName);
+
+            //button to create the character that you selected
+            JButton jbCreate = new JButton("Create!");
+            namePanel.add(jbCreate);
+
+         JPanel selectionPanel = new JPanel(new FlowLayout());
+            JButton jbPrev = new JButton("<=");
+            selectionPanel.add(jbPrev);
+
+            JButton jbNext = new JButton("=>");
+            selectionPanel.add(jbNext);
+
+         bottomPanel.add(selectionPanel);
+         bottomPanel.add(namePanel);
 
       clsFrame.add(classInfoPanel, BorderLayout.CENTER);
       clsFrame.add(bottomPanel, BorderLayout.SOUTH);
+      clsFrame.setResizable(false);
       clsFrame.setLocationRelativeTo(null);
       clsFrame.setVisible(true);
    }
