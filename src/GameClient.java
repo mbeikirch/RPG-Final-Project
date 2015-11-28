17efd0a267d813af43c68f608fecc5750e96066f
Matt Beikirch <meb3191@rit.edu>
2015-11-27 22:28:15 -0500

    Made the fight area look a little more like it actually should. No background image as of now since I can't find a proper way to do it, I'll look into it more soon.

--- src/GameClient.java
+++ src/GameClient.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.io.*;
+import javax.imageio.*;
 import java.net.*;
 import java.util.*;
 
@@ -17,9 +18,10 @@ public class GameClient extends JFrame
    PrintWriter pw;
    Player myFighter;
 
-   //Game Area
-   private Icon background = new ImageIcon(".\\media\\fightArea.png");
-
+   private Icon boss = new ImageIcon(".\\media\\boss1.png");
+   private Icon warrior = new ImageIcon(".\\media\\warrior.png");
+   private Icon wizard = new ImageIcon(".\\media\\wizard.png");
+   private Icon rogue = new ImageIcon(".\\media\\rogue.png");
 
    public static void main(String[] args)
    {
@@ -41,6 +43,7 @@ public class GameClient extends JFrame
    //Constructor
    public GameClient()
    {
+/*      //Testing code for choosing class
       String className = JOptionPane.showInputDialog(null, "Type in class(testing only)");
 
       if(className.equals("Warrior"))
@@ -54,7 +57,10 @@ public class GameClient extends JFrame
       else if(className.equals("Rogue"))
       {
          myFighter = new Rogue("Rogue");
-      }
+      }*/
+
+      myFighter = new Warrior("Matt-TESTONLY");
+
       setTitle("RPG Client");
       setResizable(false);
 
@@ -71,68 +77,103 @@ public class GameClient extends JFrame
       jpPlayerList.add(name3);
       jpPlayerList.add(name4);
 
-      //holds the fight screen, just a place holder picture for now
+      //holds the fight area. For now just placeholder images for each player and the boss
       JPanel jpFightArea = new JPanel(new FlowLayout());
-      JLabel bla = new JLabel();
-      bla.setIcon(background);
-      jpFightArea.add(bla);
+      jpFightArea.setBorder(BorderFactory.createLineBorder(Color.black,5, true));  //color, thickness of border, rounded edges
+
+         //panel to hold the boss
+         JPanel jpBossArea = new JPanel(new GridLayout(1,0,100,0));
+            JLabel jlBossImage = new JLabel();
+            jlBossImage.setIcon(boss);
+            jpBossArea.add(jlBossImage);
+            jpBossArea.add(new JLabel()); //this label is just a filler to properly space out the fighters/boss
+         jpFightArea.add(jpBossArea);
+
+         JPanel jpFighterArea = new JPanel(new GridLayout(3,2,0,5));
+            JLabel jlPlayer1 = new JLabel();
+            jlPlayer1.setIcon(warrior);
+            JLabel jlPlayer1Health = new JLabel();
+            jlPlayer1Health.setText("Health: 10/10");
+            jpFighterArea.add(jlPlayer1Health);
+            jpFighterArea.add(jlPlayer1);
+
+            JLabel jlPlayer2 = new JLabel();
+            jlPlayer2.setIcon(wizard);
+         JLabel jlPlayer2Health = new JLabel();
+         jlPlayer2Health.setText("Health: 10/10");
+         jpFighterArea.add(jlPlayer2Health);
+            jpFighterArea.add(jlPlayer2);
+
+            JLabel jlPlayer3 = new JLabel();
+            jlPlayer3.setIcon(rogue);
+         JLabel jlPlayer3Health = new JLabel();
+         jlPlayer3Health.setText("Health: 10/10");
+         jpFighterArea.add(jlPlayer3Health);
+            jpFighterArea.add(jlPlayer3);
+
+      jpFightArea.add(jpFighterArea);
 
       //holds the sending info panel, abilities panel, and text box
       JPanel jpBottom = new JPanel(new GridLayout(0,2));
-      jtaMessages = new JTextArea(6,0);
-      jtaMessages.setLineWrap(true);
-      jtaMessages.setWrapStyleWord(true);
-      jtaMessages.setEditable(false);
-      JScrollPane jspText = new JScrollPane(jtaMessages);
-      jpBottom.add(jspText);
+         jtaMessages = new JTextArea(6,0);
+         jtaMessages.setLineWrap(true);
+         jtaMessages.setWrapStyleWord(true);
+         jtaMessages.setEditable(false);
+         JScrollPane jspText = new JScrollPane(jtaMessages);
+         jpBottom.add(jspText);
 
       //holds the user text field/send button and the ability buttons
       JPanel jpBottomRight = new JPanel(new GridLayout(2,0));
 
-      //holds the ability buttons
-      JPanel jpAbilities = new JPanel(new GridLayout());
+         //holds the ability buttons
+         JPanel jpAbilities = new JPanel(new GridLayout());
 
-      //making the action listener for the abilities to use
-      AbilityListener abilityCaster = new AbilityListener();
+         //making the action listener for the abilities to use
+         AbilityListener abilityCaster = new AbilityListener();
 
-      JButton jbAbility1 = new JButton("Ability 1");
-      jbAbility1.setToolTipText("Ability 1 info");
-      jbAbility1.addActionListener(abilityCaster);
+         JButton jbAbility1 = new JButton("Ability 1");
+         jbAbility1.setToolTipText("Ability 1 info");
+         jbAbility1.addActionListener(abilityCaster);
 
-      JButton jbAbility2 = new JButton("Ability 2");
-      jbAbility2.setToolTipText("Ability 2 info");
-      jbAbility2.addActionListener(abilityCaster);
+         JButton jbAbility2 = new JButton("Ability 2");
+         jbAbility2.setToolTipText("Ability 2 info");
+         jbAbility2.addActionListener(abilityCaster);
 
-      JButton jbAbility3 = new JButton("Ability 3");
-      jbAbility3.setToolTipText("Ability 3 info");
-      jbAbility3.addActionListener(abilityCaster);
+         JButton jbAbility3 = new JButton("Ability 3");
+         jbAbility3.setToolTipText("Ability 3 info");
+         jbAbility3.addActionListener(abilityCaster);
 
-      JButton jbAbility4 = new JButton("Ability 4");
-      jbAbility4.setToolTipText("Ability 4 info");
-      jbAbility4.addActionListener(abilityCaster);
+         JButton jbAbility4 = new JButton("Ability 4");
+         jbAbility4.setToolTipText("Ability 4 info");
+         jbAbility4.addActionListener(abilityCaster);
 
-      jpAbilities.add(jbAbility1);
-      jpAbilities.add(jbAbility2);
-      jpAbilities.add(jbAbility3);
-      jpAbilities.add(jbAbility4);
-      jpBottomRight.add(jpAbilities);
+         jpAbilities.add(jbAbility1);
+         jpAbilities.add(jbAbility2);
+         jpAbilities.add(jbAbility3);
+         jpAbilities.add(jbAbility4);
+         jpBottomRight.add(jpAbilities);
 
       //holds the user text field/send button
       JPanel jpSendingInfo = new JPanel(new FlowLayout());
-      jtfSendMessage = new JTextField(25);
-      jpSendingInfo.add(jtfSendMessage);
-      JButton jbSend = new JButton("Send");
-      jbSend.addActionListener(new SendButtonListener());
-      jpSendingInfo.add(jbSend);
-      jpBottomRight.add(jpSendingInfo);
+         jtfSendMessage = new JTextField(25);
+         jpSendingInfo.add(jtfSendMessage);
+         JButton jbSend = new JButton("Send");
+         jbSend.addActionListener(new SendButtonListener());
+         jpSendingInfo.add(jbSend);
+         jpBottomRight.add(jpSendingInfo);
 
       jpBottom.add(jpBottomRight);
+
+      //this is where I try and set the background image, the testComponent class is below
+      //testComponent bla = new testComponent();
+      //bla.add(jpFightArea);
+
       add(jpFightArea, BorderLayout.CENTER);
       add(jpPlayerList, BorderLayout.EAST);
       add(jpBottom, BorderLayout.SOUTH);
 
       setDefaultCloseOperation(EXIT_ON_CLOSE);
-      setLocation(500,300);
+      setLocationRelativeTo(null);
       setVisible(true);
       jtfSendMessage.requestFocus();
 
@@ -211,5 +252,28 @@ public class GameClient extends JFrame
          catch(IOException ioe) { ioe.printStackTrace(); }
       }
    }//end inner class 3 (threadz)
+
+   /*
+   Was messing around with trying to get a background image, sort of got it to work but it wasn't working properly.
+   Ignoring for now, may try and use again later
+   */
+   public class testComponent extends JPanel
+   {
+      private Image background;
+
+      public testComponent()
+      {
+         try
+         {
+            background = ImageIO.read(getClass().getResource("fightArea.png"));
+         }
+         catch (IOException e) { e.printStackTrace(); }
+      }
+
+      public void paintComponent(Graphics g)
+      {
+         g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
+      }
+   }
 
 }//end class
