452aedafaf7bf33348c55fe6e1865b0005fb7ace
Matt Beikirch <meb3191@rit.edu>
2015-12-03 01:03:01 -0500

    Minor changes in Diablo, Rogue, Warrior, Wizard.
    Sort of phased out the player class, depending on how we do the XML stuff this may change. The way I was thinking to do the XML was just put all of the possible methods we could use for bosses/players(abilities, change healths, names, etc.) and then each specific class would have their own call to the XML parser. It makes more sense if I actually explain it in person.
    
    GameClient and GameServer now exclusively use object streams for all network traffic. I know we were initially planning to use multiple streams but this is a bit cleaner and I was running into issues with opening multiple I/O streams on each socket anyway. As of now, when the fighters connect they get added to a vector of Fighters including the boss. I'm still working on sending out the updated vector everytime something combat related happens, but that shouldn't be too bad.
    
    tl;dr Chat still works as it did before, but now we're tossing around some fancy objects.

--- src/Diablo.java
+++ src/Diablo.java
@@ -2,9 +2,7 @@ public class Diablo extends Boss
 {
    public Diablo()
    {
-      changeCurrentPower(50);
       changeBasePower(50);
-      changeCurrentHealth(100);
       changeBaseHealth(100);
    }
 }
--- src/GameServer.java
+++ src/GameServer.java
@@ -5,9 +5,13 @@ import java.util.*;
 public class GameServer
 {
    final int PORT = 4444;
-   
-   private Vector<PrintWriter> clientWriteList = new Vector<PrintWriter>();
-   private Vector<String> clientList = new Vector<String>();
+
+   //holds all of the object writers to be used
+   private Vector<ObjectOutputStream> clientWriteList = new Vector<>();
+
+   //holds all of the fighter objects(boss/4 players)
+   private Vector<Fighter> clientList = new Vector<>();
+
    protected Fighter bossMan = new Diablo();
 
    public static void main(String[] args)
@@ -18,7 +22,9 @@ public class GameServer
    public GameServer()
    {
       ServerSocket server = null;
-      
+
+      clientList.add(bossMan);
+
       try
       {
          System.out.println("Server started on IP: " + InetAddress.getLocalHost());
@@ -43,7 +49,7 @@ public class GameServer
    class ClientHandler extends Thread
    {
       Socket cs;
-      BufferedReader clientBuffer;
+      ObjectInputStream clientBuffer;
       
       public ClientHandler(Socket _cs)
       {
@@ -51,48 +57,78 @@ public class GameServer
          
          try
          {
-            //add a reader for each client as they connnect
-            clientBuffer = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+            //add an object reader for each client as they connect
+            clientBuffer = new ObjectInputStream(new DataInputStream(cs.getInputStream()));
 
-            //add client to the printwriter vector, allows chat to be sent to it
-            clientWriteList.add(new PrintWriter(cs.getOutputStream()));
+            //add client to the object writer vector
+            clientWriteList.add(new ObjectOutputStream(new DataOutputStream(cs.getOutputStream())));
 
-            //add info about the client( class, name, level, etc.) to the vector clientList
-            String charInfo = clientBuffer.readLine();
-            clientList.add(charInfo);
+            //add the entire fighter object to the clientList vector
+            clientList.add((Fighter)clientBuffer.readObject());
+            System.out.println(clientList.get(1).getClass());
+            System.out.println(clientList.get(1).getName());
+            System.out.println(clientList.get(0).getClass());
+            System.out.println(clientList.get(0).getCurrentHealth());
          }
          catch(IOException ioe){ ioe.printStackTrace(); }
+         catch(ClassNotFoundException cnfe){ cnfe.printStackTrace(); }
       }
       
       public void run()
       {
-         String message;
+         Object obj;
          
          try
          {
-            while((message = clientBuffer.readLine()) != null)
+            while((obj = clientBuffer.readObject()) != null)
             {
-               System.out.println("New message: " + message);
-               broadcastToClients(message);
-
-               bossMan.changeCurrentHealth(-25);
-
-               if(bossMan.currentHealth == 0)
+               if(obj instanceof String)
                {
-                  broadcastToClients("You win!");
+                  broadcastChatToClients((String)obj);
+
+                  System.out.println("got a string");
+               }
+
+               if(obj instanceof Vector)
+               {
+                  System.out.println("got a vector");
+
+                  clientList = (Vector)obj;
+                  broadcastFighterListToClients();
+
+                  System.out.println(clientList.get(0).getCurrentHealth());
                }
             }
          }
          catch(IOException ioe) { ioe.printStackTrace(); }
+         catch(ClassNotFoundException cnfe){ cnfe.printStackTrace(); }
       }
       
-      public void broadcastToClients(String message)
+      public void broadcastChatToClients(String message)
       {
          //print out the received message to each client that is connected
-         for(PrintWriter pw : clientWriteList)
+         for(ObjectOutputStream oos : clientWriteList)
          {
-            pw.println(message);
-            pw.flush();
+            try
+            {
+               oos.writeObject(message);
+               oos.flush();
+            }
+            catch(IOException ioe){ ioe.printStackTrace(); }
+         }
+      }
+
+      public void broadcastFighterListToClients()
+      {
+         //print out the updated client list to each client
+         for(ObjectOutputStream oos : clientWriteList)
+         {
+            try
+            {
+               oos.writeObject(clientList);
+               oos.flush();
+            }
+            catch(IOException ioe){ ioe.printStackTrace(); }
          }
       }
    }
