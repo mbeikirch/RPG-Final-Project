1e6d1d9e233f34565b55ac6b1807b2c424389dbd
Nicholas <nl8033@g.rit.edu>
2015-12-09 17:20:13 -0500

    Added tansient to the parser because it allows the implements Serializable to ignore the document builder and xpath stuff because that doesen't implement Serializable

--- src/Fighter.java
+++ src/Fighter.java
@@ -1,9 +1,11 @@
 import javax.swing.*;
+import java.io.Serializable;
 
-public class Fighter
+public class Fighter implements Serializable
 {
    protected int baseHealth, currentHealth;
    protected String name;
+   private static final long serialVersionUID = 1L;
 
    public Fighter() { }
 
