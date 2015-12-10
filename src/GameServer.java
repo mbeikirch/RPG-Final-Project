f9b079986f452fe720cb4fa96117702135257097
Matt Beikirch <meb3191@rit.edu>
2015-12-10 14:48:45 -0500

    Got the turns sort of working, will elaborate tomorrow

--- src/GameServer.java
+++ src/GameServer.java
@@ -10,11 +10,8 @@ public class GameServer
    int numPlayers;
 
    //holds all of the object writers to be used
-   private Vector<ObjectOutputStream> clientWriteList = new Vector<>();
-
-   //holds all of the fighter objects(boss/4 players)
-   private Vector<Fighter> clientList = new Vector<Fighter>();
-
+   public Vector<ObjectOutputStream> clientWriteList = new Vector<>();
+   public Vector<Fighter> clientList;
    protected Fighter bossMan = new Diablo();
 
    public static void main(String[] args)
@@ -26,12 +23,12 @@ public class GameServer
    {
       ServerSocket server = null;
 
-      clientList.add(bossMan);
-
       try
       {
          System.out.println("Server started on IP: " + InetAddress.getLocalHost());
          server = new ServerSocket(PORT);
+         clientList = new Vector<Fighter>();
+         clientList.add(bossMan);
       }
       catch(BindException be){ System.out.println("Server already running on this port..."); }
       catch(UnknownHostException uhe){ uhe.printStackTrace(); } 
@@ -79,53 +76,62 @@ public class GameServer
             clientBuffer = new ObjectInputStream(new DataInputStream(cs.getInputStream()));
 
             //add the entire fighter object to the clientList vector
-            clientList.add((Fighter)clientBuffer.readObject());
+            clientList.add((Fighter) clientBuffer.readObject());
             System.out.println("Vector Size: " + clientList.size());
             clientWriter.writeInt(clientTurnNumber++);
+            System.out.println("wrote out turn" + clientTurnNumber);
 
-            broadcastClientListToClients();
+            broadcastClientList();
 
-            if(numPlayers == 3)
+            if (numPlayers == 3)
             {
-               broadcastClientListToClients();
-               broadcastTurnNumberToClients();
+               broadcastClientList();
+               broadcastTurnNumber();
             }
          }
-         catch(EOFException | SocketException e)
+         catch (EOFException | SocketException e)
          {
             System.out.println("got a tester");
             numPlayers--;
             return;
          }
-         catch(IOException ioe){ ioe.printStackTrace(); }
-         catch(ClassNotFoundException cnfe){ cnfe.printStackTrace(); }
-         catch(NullPointerException npe){ npe.printStackTrace(); }
+         catch (IOException ioe)
+         {
+            ioe.printStackTrace();
+         }
+         catch (ClassNotFoundException cnfe)
+         {
+            cnfe.printStackTrace();
+         }
+         catch (NullPointerException npe)
+         {
+            npe.printStackTrace();
+         }
       }
-      
-      public void run()
-      {
+
+      public void run() {
          Object obj;
 
          try
          {
-            while((obj = clientBuffer.readObject()) != null)
+            while ((obj = clientBuffer.readObject()) != null)
             {
-               if(obj instanceof String)
+               if (obj instanceof String)
                {
                   System.out.println("got a string");
-                  broadcastChatToClients((String)obj);
+                  broadcastChatToClients((String) obj);
                }
                //when a vector is received, update the server's copy of the vector and broadcast it out
                //then, increment the turn number and broadcast that out
-               if(obj instanceof Vector)
+               if (obj instanceof Vector)
                {
                   System.out.println("got a vector");
 
-                  clientList = (Vector<Fighter>)obj;
-                  broadcastClientListToClients();
+                  clientList = (Vector<Fighter>) obj;
+                  broadcastClientList();
 
                   //if all the players have taken their turn(player 3 = turn 3) reset the turn counter to 0 and let the boss do stuff
-                  if(clientTurnNumber == 3)
+                  if (clientTurnNumber == 3)
                   {
                      clientTurnNumber = 0;
                      doBossStuff();
@@ -133,64 +139,71 @@ public class GameServer
                   else
                   {
                      clientTurnNumber++;
-                     broadcastTurnNumberToClients();
+                     broadcastTurnNumber();
                   }
                }
             }
          }
-         catch(SocketException se){ System.out.println("Client Disconnected"); }
-         catch(IOException ioe) { ioe.printStackTrace(); }
-         catch(ClassNotFoundException cnfe){ cnfe.printStackTrace(); }
-         catch(NullPointerException npe){ }
+         catch (SocketException se) { System.out.println("Client Disconnected"); }
+         catch (IOException ioe) { ioe.printStackTrace(); }
+         catch (ClassNotFoundException cnfe) { cnfe.printStackTrace(); }
+         catch (NullPointerException npe) { }
       }
+   }
 
-      private void doBossStuff()
-      {
-         //boss AI goes here
-         //once the boss is done, then broadcast
-         //broadcastFighterListToClients();
-         //broadcastTurnNumberToClients();
-      }
+   private void doBossStuff()
+   {
+      //boss AI goes here
+      //once the boss is done, then broadcast
+      //broadcastFighterList();
+      //broadcastTurnNumber();
+   }
 
-      public void broadcastChatToClients(String message)
+   public void broadcastChatToClients(String message)
+   {
+      //print out the received message to each client that is connected
+      for(ObjectOutputStream oos : clientWriteList)
       {
-         //print out the received message to each client that is connected
-         for(ObjectOutputStream oos : clientWriteList)
+         try
          {
-            try
-            {
-               oos.writeObject(message);
-               oos.flush();
-            }
-            catch(IOException ioe){ ioe.printStackTrace(); }
+            oos.writeObject(message);
+            oos.flush();
          }
+         catch(IOException ioe){ ioe.printStackTrace(); }
       }
+   }
 
-      public void broadcastClientListToClients()
+   public void broadcastClientList()
+   {
+      //print out the updated client list to each client
+      for(ObjectOutputStream oos : clientWriteList)
       {
-         //print out the updated client list to each client
-         for(ObjectOutputStream oos : clientWriteList)
+         try
          {
-            try
+            for(Fighter a : clientList)
             {
-               oos.writeObject(clientList);
-               oos.flush();
+               System.out.println(a.getName());
             }
-            catch(IOException ioe){ }
+            System.out.println("Client List Size: " + clientList.size());
+            oos.writeObject(clientList);
+            oos.flush();
+            System.out.println("wrote out client list of size" + clientList.size());
          }
+         catch(IOException ioe){ }
       }
+   }
 
-      public void broadcastTurnNumberToClients()
+   public void broadcastTurnNumber()
+   {
+      for(ObjectOutputStream oos : clientWriteList)
       {
-         for(ObjectOutputStream oos : clientWriteList)
+         try
          {
-            try
-            {
-               oos.writeObject(clientTurnNumber);
-               oos.flush();
-            }
-            catch(IOException ioe){ ioe.printStackTrace(); }
+            System.out.println("Current Turn: " + clientTurnNumber);
+            oos.writeObject(clientTurnNumber);
+            oos.flush();
          }
+         catch(IOException ioe){ ioe.printStackTrace(); }
       }
-   }//End class Client Handler
+   }
 }//End class file (GameServer)
